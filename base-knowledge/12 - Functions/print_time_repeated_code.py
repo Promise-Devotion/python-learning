168709fd1d034ed1e442007de1de87f29b8216a0
Promise-Devotion <sijun0224@msn.com>
2023-02-13 22:32:01 +0800

    functions

---
+++ base-knowledge/12 - Functions/print_time_repeated_code.py
@@ -0,0 +1,10 @@
+import datetime
+
+first_name = "Niyan"
+print("task completed")
+print(datetime.datetime.now())
+
+for x in range(0, 10):
+    print(x)
+print('task completed')
+print(datetime.datetime.now())
