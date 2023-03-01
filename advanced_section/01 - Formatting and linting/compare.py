698390a37efb610c9800de38de0f30972d1032e9
Promise-Devotion <sijun0224@msn.com>
2023-03-01 23:17:57 +0800

    良好的代码规范

---
+++ advanced_section/01 - Formatting and linting/compare.py
@@ -0,0 +1,10 @@
+# good formatter
+def print_hello(name: str) -> str:
+    """
+    Greets the user by name
+	  Parameters:
+		    name (str): The name of the user
+	  Returns:
+		    str: The greeting
+	  """
+    print('Hello, ' + name)
