7fafe89066dadf4ff523d4f2fc294cb221fc4af6
Promise-Devotion <sijun0224@msn.com>
2023-03-04 13:44:33 +0800

    字典操作

---
+++ base-knowledge/10 - Collections/2-Dictionaries.py
@@ -0,0 +1,14 @@
+# 字典增加属性
+tel = {'jack': 4098, 'sape': 4139}
+tel['jim'] = 5000
+
+print(tel['jack'])
+# 删除属性
+del tel['jim']
+print(list(tel))
+
+# The dict() constructor builds dictionaries directly from sequences of key-value pairs:
+result = dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
+print(result)
+# In addition, dict comprehensions can be used to create dictionaries from arbitrary key and value expressions:
+print({x: x**2 for x in (2, 4, 6)})
