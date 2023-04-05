955742370309f3813e9d26efbcf153afc864eb72
Promise-Devotion <sijun0224@msn.com>
2023-04-05 23:22:32 +0800

    csv读写

--- python_for_Data_Science/03_Read_and_write_CSV/Read_write_CSV_files.py
+++ python_for_Data_Science/03_Read_and_write_CSV/Read_write_CSV_files.py
@@ -5,5 +5,11 @@ import pandas as pd
 # print(airports_df)
 
 # Specify error_bad_lines=False to skip any rows with errors
-airports_df = pd.read_csv('airportsInvalidRows.csv', error_bad_lines=False)
+# airports_df = pd.read_csv('airportsInvalidRows.csv', error_bad_lines=False)
+# print(airports_df)
+# airports_df = pd.read_csv('airportsNoHeaderRows.csv',
+#                           header=None,
+#                           names=['Name', 'City', 'Country'])
+airports_df = pd.read_csv('airportsBlankValues.csv')
 print(airports_df)
+airports_df.to_csv('MyNewCSVFile.csv', index=False)
